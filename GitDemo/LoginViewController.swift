4c79dd45b2b1644527c714840f5818e670e4e6af
RAC IT SOLUTIONS PVT.LTD <racitsolutionspvtltd@racs-mac-mini.local>
2017-07-13 14:10:57 +0530

    Error Fix

---
+++ GitDemo/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  GitDemo
+//
+//  Created by RAC IT SOLUTIONS PVT.LTD on 13/07/17.
+//  Copyright © 2017 NetConnect. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
---
+++ GitDemo/NewFileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewFileViewController.swift
+//  GitDemo
+//
+//  Created by RAC IT SOLUTIONS PVT.LTD on 13/07/17.
+//  Copyright © 2017 NetConnect. All rights reserved.
+//
+
+import UIKit
+
+class NewFileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
